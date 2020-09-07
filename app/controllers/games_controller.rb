74a0630101505f962f6b1ff6054d0ba2ad3d3f8d
Jiawen Soh <sohjiawen@gmail.com>
2020-09-07 16:34:10 +0800

    push

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,9 @@
+class GamesController < ApplicationController
+  def new
+    @charset = Array('a'..'z')
+    @letters = Array.new(10) { @charset.sample }
+  end
+  def score
+    @word = params[:word]
+  end
+end
